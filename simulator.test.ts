f29ee502d47865ce25df3c530b86f59389371a31
kurateh <me@kurateh.com>
2025-02-11 02:32:13 +0900

    Add test

--- simulator.test.ts
+++ simulator.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "@std/assert/equals";
-import { ineffectiveClasses } from "./constants.ts";
-import { NOT, checkSubStats } from "./utils.ts";
+import { firstClasses, ineffectiveClasses } from "./constants.ts";
+import { AND, NOT, checkSubStats } from "./utils.ts";
 
 Deno.test("checkSubStats", () => {
   assertEquals(
@@ -9,6 +9,30 @@ Deno.test("checkSubStats", () => {
       "EffectiveDMG",
       "IneffectiveDMG",
     ]),
+    true
+  );
+
+  assertEquals(
+    AND(
+      NOT(checkSubStats(ineffectiveClasses, 2)),
+      checkSubStats(firstClasses)
+    )(["IneffectiveDMG", "IneffectiveDMG", "CritDmg"]),
+    false
+  );
+
+  assertEquals(
+    AND(
+      NOT(checkSubStats(ineffectiveClasses, 2)),
+      checkSubStats(firstClasses)
+    )(["IneffectiveDMG", "ATK%", "EffectiveDMG"]),
+    false
+  );
+
+  assertEquals(
+    AND(
+      NOT(checkSubStats(ineffectiveClasses, 2)),
+      checkSubStats(firstClasses)
+    )(["IneffectiveDMG", "ATK%", "CritDmg"]),
     true
   );
 });
